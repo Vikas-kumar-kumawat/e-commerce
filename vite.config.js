a30b666e7688dc8bda8ebe9751cd76db3c86c1eb
vikas <kvikaskumar040@gamil.com>
2025-03-28 01:21:17 +0530

    firs

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss()
-
-  ],
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // नेटवर्क पर एक्सपोज़ करने के लिए
+    port: 5173,       // कस्टम पोर्ट (इसे बदल सकते हैं)
+    strictPort: true, // अगर पोर्ट बिजी हुआ तो फेल कर देगा, कोई और पोर्ट नहीं चुनेगा
+  }
 })
